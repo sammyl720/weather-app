884e7b71e36cbdaa1d4a6267d9d30ba337ef0099
sam <sam.leider@hotmail.com>
2020-11-15 17:42:25 -0500

    changed input layout

--- src/components/AddCity/AddCity.tsx
+++ src/components/AddCity/AddCity.tsx
@@ -1,16 +1,20 @@
-import React, {useState, useContext } from 'react'
+import React, {useState, useContext, useRef } from 'react'
 import LocationContext from '../../context/locationContext/LocationContext'
 import classes from './AddCity.module.scss'
 const AddCity = () => {
   const {  addZipcode } = useContext(LocationContext)
   const [zip, setZip] = useState('')
+  const inputRef = useRef<HTMLInputElement>(null)
   const onSubmit = e => {
     const zipReg = /^\d{5}$/
     e.preventDefault()
-
     if(zipReg.test(zip)){
       console.log(`${zip} add`)
       addZipcode(zip)
+      setZip('')
+      if(null !== inputRef.current){
+        inputRef.current.blur()
+      }
     } else {
       alert('Please enter a valid zipcode')
     }
@@ -22,7 +26,9 @@ const AddCity = () => {
         pattern='^\d{5}$'
         value={zip}
         placeholder='Zipcode'
+        inputMode='numeric'
         name='zipcode'
+        ref={inputRef}
         autoComplete='false'
         onChange={e => setZip(e.target.value)} 
       />
